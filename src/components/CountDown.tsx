fc53af18c69d46d130fc5e6d3c8bffa61cf18815
Alison Ramos da Silva <alison.silva@dbserver.com.br>
2021-02-24 12:15:32 -0300

    Usando useState e useEffect para o incrementTimeClock

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,22 +1,56 @@
+import {useState, useEffect} from "react";
 import styles from '../styles/components/CountDown.module.css'
 
 export function CountDown() {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);//arredonda para baixo caso venha hora quebrada ex: 24.59 = 24
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = splitTime(minutes);
+  const [secondLeft, secondRight] = splitTime(seconds);
+
+  function startCountDown() {
+    setActive(true);
+  }
+
+  function incrementTimeClock() {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000)
+    }
+  }
+
+//passando o active ou time como dependencia do useEffect quando o valor mudar, a funcao sera executada
+  useEffect(() => {
+    incrementTimeClock();
+  }, [active, time])
+
   return (
     <div>
       <div className={styles.countdownContainer}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
-      <button type={"button"} className={styles.countdownButton}>
+      <button type={"button"}
+              className={styles.countdownButton}
+              onClick={startCountDown}>
         Iniciar um ciclo
       </button>
     </div>
   );
+}
+
+function splitTime(minutesOrSeconds: number): string[] {
+  //padstart caso não tenhha 2 character ele joga zero a frente da string e o split reparte.
+  return String(minutesOrSeconds).padStart(2, '0').split('');
 }
