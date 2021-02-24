42145d4f2b46b79dc43b5d8dafb3502f99a2378e
Alison Ramos da Silva <alison.silva@dbserver.com.br>
2021-02-24 15:10:39 -0300

    Condicao ternaria botao ciclo concluido

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -4,8 +4,9 @@ import styles from '../styles/components/CountDown.module.css'
 let countdownTimeout: NodeJS.Timeout;
 
 export function CountDown() {
-  const [time, setTime] = useState(25 * 60);
+  const [time, setTime] = useState(0.1 * 60);
   const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time / 60);//arredonda para baixo caso venha hora quebrada ex: 24.59 = 24
   const seconds = time % 60;
@@ -21,7 +22,7 @@ export function CountDown() {
     //cancelando execucao do timeout ao clicar no abandonar ciclo
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(25*60);
+    setTime(0.1 * 60);
   }
 
   function incrementTimeClock() {
@@ -29,6 +30,9 @@ export function CountDown() {
       countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000)
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
     }
   }
 
@@ -52,18 +56,32 @@ export function CountDown() {
         </div>
       </div>
 
-      {isActive ? (
-        <button type={"button"}
-                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
-                onClick={resetCountDown}>
-          Abandonar ciclo
+      {/* Estrutura de como fazer um if ternario no react
+        { condicao ? (true) : ( <> (false) </> )}
+      */}
+
+      {hasFinished ? (
+        <button disabled className={styles.countdownButton}>
+          Ciclo encerrado
         </button>
       ) : (
-        <button type={"button"}
-                className={styles.countdownButton}
-                onClick={startCountDown}>
-          Iniciar um ciclo
-        </button>
+
+        <> {/* Fragment, é como se fosse uma div no html ela nao e exibida.*/}
+
+          {isActive ? (
+            <button type={"button"}
+                    className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                    onClick={resetCountDown}>
+              Abandonar ciclo
+            </button>
+          ) : (
+            <button type={"button"}
+                    className={styles.countdownButton}
+                    onClick={startCountDown}>
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
       )}
     </div>
   );
