a19ecb0e73a299a3b9eb88aa330bc2b07084c110
Alison Ramos da Silva <alison.silva@dbserver.com.br>
2021-02-28 15:15:53 -0300

    Criada Modal Level UP

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,6 +1,7 @@
 import {createContext, ReactNode, useEffect, useState} from 'react';
 import challenges from '../../challenges.json';
 import Cookies from 'js-cookie';
+import {LevelUpModal} from "../components/LevelUpModal";
 
 interface Challenge {
   type: 'body' | 'eye';
@@ -105,6 +106,8 @@ export function ChallengesProvider({children, ...rest}: ChallengesProviderProps)
         completeChallenge
       }}>
       {children}
+
+      <LevelUpModal/>
     </ChallengesContext.Provider>
   );
 
